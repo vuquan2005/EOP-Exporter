236c94b850d6b560a6f5356bb0d1bc2daeee41de
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-03-11 00:52:51 +0700

    Clean code

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,12 @@
-//event click on the button
+const textarea = document.getElementById('data');
+
+const autoCopy = () => {
+    textarea.select();
+    document.execCommand('copy');
+};
+
+// Click button Export New Word
 document.getElementById('exportNewWord').addEventListener('click', () => {
-    
-    //send message click to content.js
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {action: "clickBtnExportNewWord"});
     });
@@ -15,24 +20,23 @@ document.getElementById('exportNewWord').addEventListener('click', () => {
 // });
 //chrome.runtime.sendMessage({action: 'showData', data: dataArr});
 
-
-// listen for messages from the content script and run show data to textarea
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     if (msg.action === 'exportNewWord') {
-        // get the data from the message
         const data = msg.data;
-        // create a variable to store the data
         let text = '';
-        // loop through the data and add it to the text
         data.forEach((item) => {
             //create cloze replace all character in newWord to _, no replace the first character and space
             const cloze = item.newWord.replace(/[^ ]/g, '_');
-            // add the data to the text
             text += `${item.newWord};${cloze};${item.phonetic};${item.meaning};${item.example}\n`;
         });
-        // add the text to the textarea
-        textarea.value = text;
+        textarea.value = text += '\n';
+        autoCopy();
     }
 });
 
-const textarea = document.getElementById('data');
+// Click button Export Vocabulary
+document.getElementById('exportVocabulary').addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "clickBtnExportVocabulary"});
+    });
+});
