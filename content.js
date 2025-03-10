8620c5ebbae240fe69a130e18b8554961b3fec20
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-03-11 01:00:01 +0700

    add event, message

--- content.js
+++ content.js
@@ -4,7 +4,21 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     }
 });
 
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.action === 'clickBtnExportVocabulary') {
+        exportVocabulary();
+    }
+});
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.action === 'clickBtnExportGrammar') {
+        exportGrammar();
+    }
+});
+
+
 const exportNewWord = () => {
+    //<div class="ditem"> <h4><i class="fa fa-play-circle daudio" rel="e201bb3c371fd1abec42" media-url="//c.eop.edu.vn/desktop/dictionary/1849/462d5104332t5737l0.mp3"></i>&nbsp;Band saw/ bandsaw</h4> <div class="minhhoa"> <i>/ˈbændsɔː/</i> <img src="//c.eop.edu.vn/media/auto/120/dictionary/1849/462d5104328t6037l6.png" style="border: 1px solid rgb(254, 254, 254);"> </div> <p><b>Cưa máy</b></p><br> <p>The most common use for the band saw is in cutting irregular shapes.</p> </div>
     const data = document.querySelectorAll('div.ditem');
     const dataArr = [];
     data.forEach((item) => {
@@ -30,6 +44,25 @@ const exportNewWord = () => {
 
     chrome.runtime.sendMessage({action: 'exportNewWord', data: dataArr});
 };
-    //<div class="ditem"> <h4><i class="fa fa-play-circle daudio" rel="e201bb3c371fd1abec42" media-url="//c.eop.edu.vn/desktop/dictionary/1849/462d5104332t5737l0.mp3"></i>&nbsp;Band saw/ bandsaw</h4> <div class="minhhoa"> <i>/ˈbændsɔː/</i> <img src="//c.eop.edu.vn/media/auto/120/dictionary/1849/462d5104328t6037l6.png" style="border: 1px solid rgb(254, 254, 254);"> </div> <p><b>Cưa máy</b></p><br> <p>The most common use for the band saw is in cutting irregular shapes.</p> </div>
 
+const exportVocabulary = () => {
+    const data = document.querySelectorAll('div.ditem');
+    const dataArr = [];
+    data.forEach((item) => {
+        
+    });
+    console.log(dataArr);
 
+    chrome.runtime.sendMessage({action: 'exportVocabulary', data: dataArr});
+};
+
+const exportGrammar = () => {
+    const data = document.querySelectorAll('div.ditem');
+    const dataArr = [];
+    data.forEach((item) => {
+        
+    });
+    console.log(dataArr);
+
+    chrome.runtime.sendMessage({action: 'exportGrammar', data: dataArr});
+}
--- popup/popup.js
+++ popup/popup.js
@@ -1,42 +1,76 @@
-const textarea = document.getElementById('data');
+const textarea = document.getElementById("data");
 
 const autoCopy = () => {
-    textarea.select();
-    document.execCommand('copy');
+	textarea.select();
+	document.execCommand("copy");
 };
 
 // Click button Export New Word
-document.getElementById('exportNewWord').addEventListener('click', () => {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "clickBtnExportNewWord"});
-    });
+document.getElementById("exportNewWord").addEventListener("click", () => {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {
+			action: "clickBtnExportNewWord",
+		});
+	});
 });
 
-// dataArr.push({
-//     newWord,
-//     phonetic,
-//     meaning,
-//     example
-// });
-//chrome.runtime.sendMessage({action: 'showData', data: dataArr});
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+	if (msg.action === "exportNewWord") {
+		const data = msg.data;
+		// dataArr.push({
+		//     newWord,
+		//     phonetic,
+		//     meaning,
+		//     example
+		// });
+		//chrome.runtime.sendMessage({action: 'showData', data: dataArr});
+		let text = "";
+		data.forEach((item) => {
+			//create cloze replace all character in newWord to _, no replace the first character and space
+			const cloze = item.newWord.replace(/[^ ]/g, "_");
+			text += `${item.newWord};${cloze};${item.phonetic};${item.meaning};${item.example}\n`;
+		});
+		textarea.value = text += "\n";
+		autoCopy();
+	}
+});
+
+// Click button Export Vocabulary
+document.getElementById("exportVocabulary").addEventListener("click", () => {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {
+			action: "clickBtnExportVocabulary",
+		});
+	});
+});
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if (msg.action === 'exportNewWord') {
+    if (msg.action === "exportVocabulary") {
         const data = msg.data;
-        let text = '';
-        data.forEach((item) => {
-            //create cloze replace all character in newWord to _, no replace the first character and space
-            const cloze = item.newWord.replace(/[^ ]/g, '_');
-            text += `${item.newWord};${cloze};${item.phonetic};${item.meaning};${item.example}\n`;
-        });
-        textarea.value = text += '\n';
+        
+        let text = "";
+        
+        textarea.value = text += "\n";
         autoCopy();
     }
 });
 
-// Click button Export Vocabulary
-document.getElementById('exportVocabulary').addEventListener('click', () => {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "clickBtnExportVocabulary"});
-    });
+// Click button Export Grammar
+document.getElementById("exportGrammar").addEventListener("click", () => {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {
+			action: "clickBtnExportGrammar",
+		});
+	});
+});
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.action === "exportGrammar") {
+        const data = msg.data;
+        
+        let text = "";
+        
+        textarea.value = text += "\n";
+        autoCopy();
+    }
 });
