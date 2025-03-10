836d1da294c01a0c89fd7f73bcd82fa81536d767
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-03-10 23:40:43 +0700

    Export new word

---
+++ content.js
@@ -0,0 +1,51 @@
+// listen for messages from the popup and run the function
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.action === 'clickBtnExportNewWord') {
+        exportNewWord();
+    }
+});
+
+// arow function to export data
+const exportNewWord = () => {
+    // get all the data from the page
+    const data = document.querySelectorAll('div.ditem');
+
+    // create an array to store the data
+    const dataArr = [];
+    // loop through the data and push it to the array
+    data.forEach((item) => {
+        // get the text content from h4 tags
+        let newWord = item.querySelector('h4').textContent;
+        // remove the firt character from the text content
+        newWord = newWord.slice(1);
+
+        // get the text content from div.minhhoa tags
+        let phonetic = item.querySelector('div.minhhoa').querySelector('i').textContent;
+        // remove the first and last character from the text content
+        phonetic = phonetic.slice(1, -1);
+
+        // get the text content from b tags
+        let meaning = item.querySelector('b').textContent;
+        // get the text content from p tags
+        let example = item.querySelectorAll('p')[1].textContent;
+
+        // push the data to the array
+        dataArr.push({
+            newWord,
+            phonetic,
+            meaning,
+            example
+        });
+    });
+    
+    //log the data to the console
+    console.log(dataArr);
+
+    //send the data to the popup
+    //send message to popup.js
+    chrome.runtime.sendMessage({action: 'exportNewWord', data: dataArr});
+
+
+    //<div class="ditem"> <h4><i class="fa fa-play-circle daudio" rel="e201bb3c371fd1abec42" media-url="//c.eop.edu.vn/desktop/dictionary/1849/462d5104332t5737l0.mp3"></i>&nbsp;Band saw/ bandsaw</h4> <div class="minhhoa"> <i>/ˈbændsɔː/</i> <img src="//c.eop.edu.vn/media/auto/120/dictionary/1849/462d5104328t6037l6.png" style="border: 1px solid rgb(254, 254, 254);"> </div> <p><b>Cưa máy</b></p><br> <p>The most common use for the band saw is in cutting irregular shapes.</p> </div>
+};
+
--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,38 @@
-document.getElementById("taskClock").addEventListener("click", function() {
-    chrome.runtime.sendMessage({message: "taskClock"});
+//event click on the button
+document.getElementById('exportNewWord').addEventListener('click', () => {
+    
+    //send message click to content.js
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "clickBtnExportNewWord"});
+    });
 });
 
-document.getElementById("sessionClock").addEventListener("click", function() {
-    chrome.runtime.sendMessage({message: "sessionClock"});
+// dataArr.push({
+//     newWord,
+//     phonetic,
+//     meaning,
+//     example
+// });
+//chrome.runtime.sendMessage({action: 'showData', data: dataArr});
+
+
+// listen for messages from the content script and run show data to textarea
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.action === 'exportNewWord') {
+        // get the data from the message
+        const data = msg.data;
+        // create a variable to store the data
+        let text = '';
+        // loop through the data and add it to the text
+        data.forEach((item) => {
+            //create cloze replace all character in newWord to _, no replace the first character and space
+            const cloze = item.newWord.replace(/[^ ]/g, '_');
+            // add the data to the text
+            text += `${item.newWord};${cloze};${item.phonetic};${item.meaning};${item.example}\n`;
+        });
+        // add the text to the textarea
+        textarea.value = text;
+    }
 });
 
-document.getElementById("totalClock").addEventListener("click", function() {
-    chrome.runtime.sendMessage({message: "totalClock"});
-});
+const textarea = document.getElementById('data');
